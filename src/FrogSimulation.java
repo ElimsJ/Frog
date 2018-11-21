99c3790c8070c6053392b3eb1a57e2f9c73d31d6
BT_1N3_25 <sjiang9453@bths.edu>
2018-11-21 10:12:41 -0500

    MAX HOP

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -4,10 +4,13 @@ public class FrogSimulation
     private int maxHops;
     int total;
     double ratio;
+
     public FrogSimulation (int dist, int numHops)
     {
+
         goalDistance = dist;
         maxHops = numHops;
+
     }
     private int hopDistance()
     {
@@ -15,23 +18,30 @@ public class FrogSimulation
     }
     public boolean simulate()
     {
+
         for(int i = 0; i<maxHops; i++)
         {
             hopDistance();
             System.out.println(hopDistance());
             total =  total + hopDistance();
         }
+
         if( total >= goalDistance)
         {
+            System.out.println(goalDistance);
             return true;
         }
+
         else
         {
+            System.out.println(maxHops);
             return false;
         }
+
     }
     public double runSimulation(int num)
     {
+
         for( int i = 0; i < num; i++)
         {
             simulate();
--- src/Runner.java
+++ src/Runner.java
@@ -5,6 +5,7 @@ public class Runner
         FrogSimulation frog1 = new FrogSimulation(5, 3);
         FrogSimulation frog2 = new FrogSimulation(5, 3);
         FrogSimulation frog3 = new FrogSimulation(15, 3);
+
         System.out.println("Frog 1 Success Rate : " + frog1.runSimulation(2));
         System.out.println("Frog 2 Success Rate : " + frog2.runSimulation(2));
         System.out.println("Frog 3 Success Rate : " + frog3.runSimulation(2));
